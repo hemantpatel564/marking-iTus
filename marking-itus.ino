bd52adc1c9a7bd55cf4fdc72dbcb417f2dff3b94
hemantpatel564 <54856451+hemantpatel564@users.noreply.github.com>
2019-11-07 13:54:36 -0500

    Update marking-itus.ino

--- marking-itus.ino
+++ marking-itus.ino
@@ -1,6 +1,7 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <InternetButton.h>
 
+
 InternetButton b = InternetButton();
 
 void setup() {
@@ -19,65 +20,64 @@ void loop() {
 int showFaces(String time)
  {
       int t = time.toInt();
-      
-    if(t = 0)
+    if(t == 0)
     {
      
      b.allLedsOff();
-     b.ledOn(1,255,0,0);
-     b.ledOn(11,255,0,0);
-     b.ledOn(3,255,0,0);
-     b.ledOn(4,255,0,0);
-     b.ledOn(5,255,0,0);
-     b.ledOn(6,255,0,0);
-     b.ledOn(7,255,0,0);
-     b.ledOn(8,255,0,0);
-     b.ledOn(9,255,0,0);
+     b.ledOn(1,0,255,0);
+     b.ledOn(11,0,255,0);
+     b.ledOn(3,0,255,0);
+     b.ledOn(4,0,255,0);
+     b.ledOn(5,0,255,0);
+     b.ledOn(6,0,255,0);
+     b.ledOn(7,0,255,0);
+     b.ledOn(8,0,255,0);
+     b.ledOn(9,0,255,0);
     }
     
-    else if(t=4)
+    else if(t == 4)
     {
     
      b.allLedsOff();
-     b.ledOn(1,255,0,0);
-     b.ledOn(11,255,0,0);
-     b.ledOn(4,255,0,0);
-     b.ledOn(5,255,0,0);
-     b.ledOn(6,255,0,0);
-     b.ledOn(7,255,0,0);
-     b.ledOn(8,255,0,0);
+     b.ledOn(1,0,255,0);
+     b.ledOn(11,0,255,0);
+     b.ledOn(4,0,255,0);
+     b.ledOn(5,0,255,0);
+     b.ledOn(6,0,255,0);
+     b.ledOn(7,0,255,0);
+     b.ledOn(8,0,255,0);
   
     }
          
-    else if(t=8)
+    else if(t == 8)
         {
             b.allLedsOff();
             
-         b.ledOn(1,255,0,0);
-         b.ledOn(11,255,0,0);
-        b.ledOn(5,255,0,0);
-        b.ledOn(6,255,0,0);
-        b.ledOn(7,255,0,0);
+         b.ledOn(1,0,255,0);
+         b.ledOn(11,0,255,0);
+        b.ledOn(5,0,255,0);
+        b.ledOn(6,0,255,0);
+        b.ledOn(7,0,255,0);
             
         }
         
           
           
-      else if(t=12)
+      else if(t == 12)
         {
             
             b.allLedsOff();
             
-         b.ledOn(1,255,0,0);
-         b.ledOn(11,255,0,0);
-         b.ledOn(6,255,0,0);
+         b.ledOn(1,0,255,0);
+         b.ledOn(11,0,255,0);
+         b.ledOn(6,0,255,0);
         }
         
-        else if(t=16)
+        else if(t == 16)
         {
             b.allLedsOff();
-         b.ledOn(1,255,0,0);
-         b.ledOn(11,255,0,0);
+         b.ledOn(1,0,255,0);
+         b.ledOn(11,0,255,0);
         }
         
         else 
@@ -86,3 +86,4 @@ int showFaces(String time)
         }
 
  }
+
